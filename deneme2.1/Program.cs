244714202287f3485a8b55569458d8a1793b50db
Hüseyin Bağlayıcı <54378719+huseyinbaglayici@users.noreply.github.com>
2023-11-19 20:25:29 +0300

    Add files via upload

---
+++ deneme2.1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Kalma Süresini Giriniz : ");
+int girilenSaat = Convert.ToInt32(Console.ReadLine());
+int gunKaldi = girilenSaat / 24 + 1;
+int ucret = 0 ;
+
+for (int i = 0 ; i < gunKaldi ; i++ )
+{
+   int gunlukSaat = girilenSaat - i * 24 ;
+   gunlukSaat = gunlukSaat < 24 ? gunlukSaat : 24;
+   if (gunlukSaat>2)
+   {
+      ucret+= 5;
+      gunlukSaat -= 2;
+   }
+   if (gunlukSaat<=8)
+   ucret += gunlukSaat * 1 ;
+   else
+    ucret += 8;
+}
+Console.WriteLine("Toplam = " + ucret + "TL");
+
