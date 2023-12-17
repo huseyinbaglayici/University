64285937a33d25ce224909922c7b7de18ef7bab0
Hüseyin Bağlayıcı <54378719+huseyinbaglayici@users.noreply.github.com>
2023-12-17 17:08:35 +0300

    Add files via upload

---
+++ calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write ("1. sayıyı giriniz!:");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Toplama için   (+)= ");
+Console.WriteLine("Çıkarma   için (-)= ");
+Console.WriteLine("Çarpma   için  (*)= ");
+Console.WriteLine("Bölme   için   (/)= ");
+Console.WriteLine("Mod Alma  için (%)= ");
+string islem = Console.ReadLine()?? "İşlem boş olamaz";
+
+Console.WriteLine("2. sayıyı giriniz!:");
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+double sonuc = 0;
+if(islem=="+")    {
+     sonuc = sayi1+sayi2;
+}
+else if (islem=="-")  {
+     sonuc = sayi1-sayi2;
+}
+else if (islem =="*") {
+     sonuc = sayi1*sayi2;
+}
+else if (islem =="/" ) {
+     sonuc = sayi1/sayi2;
+}
+else if (islem =="%"){
+     sonuc = sayi1%sayi2;
+}
+else {
+    Console.WriteLine("Geçersiz İşlem Değeri girdiniz !");
+}
+Console.Write("=" + sonuc);
