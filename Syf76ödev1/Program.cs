1b6872ca857b7b2e6a20dd9d39b8b177ada968c2
Hüseyin Bağlayıcı <54378719+huseyinbaglayici@users.noreply.github.com>
2024-01-14 22:39:16 +0300

    Add files via upload

---
+++ Syf76ödev1/Program.cs
@@ -0,0 +1,22 @@
+﻿//sayfa 76 Ödev1 
+//Öyle bir program yazın ki kullanıcının klavyeden girdiği sayıları toplasın kullanıcı klavyeden 83 girerse toplama işlemi bitsin ve ekrana yazdırılsın
+
+Console.WriteLine("Kaç kez sayı yazacağınızı giriniz");
+int girilenMiktar = Convert.ToInt32(Console.ReadLine());
+int toplam = 0;
+for (int i = 1; i <= girilenMiktar; i++)
+{
+    Console.WriteLine("Sayıları giriniz.");
+    int girilen = Convert.ToInt32(Console.ReadLine());
+    if (girilen != 83)
+    {
+        toplam += girilen;
+    }
+    else if (girilen == 83)
+    {
+        toplam += 83;
+        Console.WriteLine("Sayı olarak 83 yazdığınız için işlem sona erdi!");
+        break;
+    }
+}
+Console.WriteLine("İşlemin Sonucu=" + toplam);
