5902d743f03dc2e267e9b38e9ff624afd3c4a4d3
Hüseyin Bağlayıcı <54378719+huseyinbaglayici@users.noreply.github.com>
2024-01-14 22:44:27 +0300

    Add files via upload

---
+++ syf81ödev2/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{//B değerinin asal olup olmadığını bulan program!
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bu program girdiğiniz sayının asal olup olmadığını kontrol eder!\n Bir sayı giriniz: ");
+            int b  = Convert.ToInt32(Console.ReadLine());
+            int a; // B=11
+            string s = "girdiğiniz sayı asal sayı";
+            if (b==1) { Console.Write("'1' Asal Değildir!"); goto bitir; }
+            for (a = 2;  a<=b /2; a++)
+            {
+                if (b%a==0)
+                {
+                    s = "girdiğiniz sayı asal sayı değildir!";
+                    break;
+                } 
+            }
+            Console.WriteLine(s);
+        bitir: Console.Write("DEVAM ETMEK İÇİN BİR TUŞA BASINIZ...");
+            Console.ReadKey(true);
+            
+        }
+    }
+}
