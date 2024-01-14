92cb5a3a76780ae9d76bb55c5eabd27578bc4e36
Hüseyin Bağlayıcı <54378719+huseyinbaglayici@users.noreply.github.com>
2024-01-14 22:40:03 +0300

    Add files via upload

---
+++ syf76ödev3/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcı iki sayı girsin iki sayı arasından isterse çiftleri isterse teklerin toplamını ekrana yazdırsın
+            // kullanıcıya seçenek verilsin!
+            // SAYFA 76 ödev3
+            Console.WriteLine("Küçük sayıyı giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Büyük sayıyı giriniz:");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Tek sayılar mı bulunsun Çift mi ?:");
+            string tercih=(Console.ReadLine());
+            int toplam = 0;
+            if(tercih == "çift")
+            {
+                for (int i = sayi; i<= sayi2; i++)
+                    if (i % 2 == 0) { 
+                        toplam += i;
+                    }
+            }
+            else if (tercih == "tek")
+            {
+                for (int i = sayi; i<=sayi2; i++)
+                {
+                    if (i % 2 == 1)
+                    {
+                        toplam += i;
+                    }
+                }
+            }
+            Console.WriteLine(toplam);
+            Console.ReadLine();
+
+        }
+    }
+}
